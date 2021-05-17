383c31b555212e4c14f253582a1bb585005a4199
Priyankaraj107 <>
2021-05-17 11:48:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+your_expected_age=input("what is your expected age to die?")
+#Write your code below this line 👇
+years=int(your_expected_age)-int(age)
+months=years*12
+weeks=years*52
+days=years*365
+
+print(f"You have {days} days, {weeks} weeks,and {months} months left" )
+
+
+
+
+
+
+
+
+
